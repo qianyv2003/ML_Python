4debd6e4868cd074b2d13612d2b40c35ef63f90b
karibu nyahua <karibu@karibus-MacBook-Pro.local>
2023-03-02 21:35:42 +0800

    gpt api

---
+++ nyahua/chat.py
@@ -0,0 +1,13 @@
+import openai
+openai.api_key = "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"
+chat = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+reply = chat.choices[0].message.content
+print(reply)
